013b509c98cf90fd703c7b79efb29d2f8f5646cf
Nurmuhammet Allanov <nurmuhammetali2000@gmail.com>
2022-05-07 00:53:21 +0500

    add fucker

---
+++ Dynamicjson/src/Dynamicjson.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace NurmuhammetAli\Dynamicjson;
+
+use Laravel\Nova\Fields\Field;
+
+class Dynamicjson extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'dynamicjson';
+}
